104930a5f239402047d4b91032e18955cf63f6f5
idealtrue0 <dhflcl@naver.com>
2024-07-30 21:14:07 +0900

    gwon-yongjin 님이 변경한 내용

---
+++ src/main/java/com/project/FreeCycle/Repository/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.project.FreeCycle.Repository;
+
+public interface ProductRepository {
+}
--- src/main/java/com/project/FreeCycle/Service/ProductService.java
+++ src/main/java/com/project/FreeCycle/Service/ProductService.java
@@ -1,8 +1,18 @@
 package com.project.FreeCycle.Service;
 
+import com.project.FreeCycle.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+    
 
 }
