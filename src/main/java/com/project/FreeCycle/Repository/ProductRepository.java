9293978ae29ae8de831815deeaecc333ca7f5ac4
idealtrue0 <dhflcl@naver.com>
2024-07-31 15:56:04 +0900

    abc

--- src/main/java/com/project/FreeCycle/Repository/ProductRepository.java
+++ src/main/java/com/project/FreeCycle/Repository/ProductRepository.java
@@ -1,6 +1,17 @@
 package com.project.FreeCycle.Repository;
 
+import com.project.FreeCycle.Domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 
-public interface ProductRepository extends JpaRepository {
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    Optional<Product> findByName(String name);
+    Optional<Product> findByContent(String content);
+    Optional<Product> findByUpload_time(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDateTime upload_time);
+
+
+
 }
