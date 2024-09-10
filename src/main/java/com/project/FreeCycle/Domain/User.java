7c529caa4d7ea743a087ff366d57a17ef44a6a31
Choi-89 <wlghks3418@naver.com>
2024-09-10 22:03:40 +0900

    이전 채팅 보이기, 채팅방 상대방 이름 보이기, 채팅방 삭제, 채팅방 추가

--- src/main/java/com/project/FreeCycle/Domain/User.java
+++ src/main/java/com/project/FreeCycle/Domain/User.java
@@ -51,6 +51,10 @@ public class User {
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Product> products = new ArrayList<>();
 
+    //유저가 가지고있는 채팅
+    @OneToMany(mappedBy="user", fetch=FetchType.LAZY)
+    private List<Chat> chats = new ArrayList<>();
+
     public String getPhoneNum(){
         try{
             return AESUtil.decrypt(phoneNum);
